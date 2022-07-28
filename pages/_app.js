e8c8b19ca864bc19fb7540e0f1e50c943ab814aa
Rohan <Rohan.khora007@gmail.com>
2022-07-28 22:58:06 +0530

    28

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import '../styles/navbar.css'
 import '../styles/globals.css'
+import Image from "next/image";
+import Navbar from './navbar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return<>
+  <Navbar/>
+  <Component {...pageProps} />
+  </> 
 }
 
 export default MyApp
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "../styles/Home.module.css";
+
+
+const about = () => {
+  return (
+    <div className={styles.menu}><h2>About Us</h2></div>
+  )
+}
+
+export default about
