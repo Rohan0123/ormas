c11e97a8b734ff5b98379fb0cad8faa597fe79da
Rohan <Rohan.khora007@gmail.com>
2022-07-31 23:15:52 +0530

    31

--- pages/exhibition.js
+++ pages/exhibition.js
@@ -3,7 +3,12 @@ import styles from "../styles/Home.module.css";
 
 const exhibition = () => {
   return (
-    <div className={styles.menu} ><h2>Exhibitions:</h2></div>
+    <div className={styles.menu} >
+      <h2>Exhibitions:</h2>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
   )
 }
 
---
+++ pages/tail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const tail = () => {
+  return (
+    <div>
+        tail
+        <h1 className="text-8xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default tail
